0ae943874d4825eabe3122a40782dc1da50081b5
edoka <peter.edoka.242464@unn.edu.ng>
2023-09-14 04:46:14 +0100

    updated models and migrations

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $casts= [
+        'body' => 'array',
+        'updated_at' => 'datetime'
+    ];
+
 }
