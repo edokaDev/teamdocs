4d6a44778b6440fef2c743431c06c141c3917937
edoka <peter.edoka.242464@unn.edu.ng>
2023-09-20 09:16:08 +0100

    install composer on with start script

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+echo 'Installing composer'
+RUN curl -sS https://getcomposer.org/installer | php -- \
+     --install-dir=/usr/local/bin --filename=composer
+
+COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+
 echo 'Running composer'
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
