7e560041d65b7408faa255294ec3b31f5d6651d1
edoka <peter.edoka.242464@unn.edu.ng>
2023-09-20 09:53:38 +0100

    updated start script: php8.2-pgsql

--- start.sh
+++ start.sh
@@ -4,9 +4,10 @@ echo 'Installing composer'
 curl -sS https://getcomposer.org/installer | php -- \
      --install-dir=/usr/local/bin --filename=composer
 
+echo 'Installing helper packages'
 apt-get install -y zip
 apt-get install -y git
-apt-get install -y php-pgsql
+apt-get install -y php8.2-pgsql
 
 echo 'Running composer'
 composer install --no-dev --working-dir=/var/www/html
